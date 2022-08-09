d4d9d5ced385e419abe9c60d9535dbe6b8b9f111
Some Guy <msanderson@vmware.com>
2022-08-09 15:56:29 +1000

    Respect the PORT environment variable in the app code.

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def hello_world():
     return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
-    app.run(debug=True,host='127.0.0.1',port=8080)
+    app.run(debug=True,host='127.0.0.1',port=int(os.getenv("PORT", "8080")))
