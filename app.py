b177921a168c02ba13f45431d7edcfc0c58668bc
Some Guy <msanderson@vmware.com>
2022-08-25 18:30:45 +1000

    Attempt to output the user's chosen ice cream flavour.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     target = os.environ.get('TARGET', 'World')
-    return 'Hello {}!\n'.format(target)
+    return 'Hello {}! Do you like ${iceCreamFlavour} ice cream?\n'.format(target)
 
 if __name__ == "__main__":
     app.run(debug=True,host='127.0.0.1',port=int(os.getenv("PORT", "8080")))
